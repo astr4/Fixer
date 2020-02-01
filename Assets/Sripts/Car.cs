09e22ae32cf8dfe0e89290b576557a27addc31d3
Ayberk Ortancalar <ortancalarayberk@gmail.com>
2020-02-01 09:52:33 +0300

    onTheWay
    there is more to do

---
+++ Assets/Sripts/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("barricade"))
+        {
+            speed = 0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(2 * Time.deltaTime * speed, 0,0);
+        transform.localScale = new Vector2(2, 2);
+    }
+}
